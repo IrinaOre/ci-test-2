f3cebc8b027a557e8582b4cbbc838aabf165a1a2
Orekhova <irinamarkina803@gmail.com>
2024-12-12 15:54:03 +0400

    init

---
+++ __test__/health.test.js
@@ -0,0 +1,16 @@
+import { sortArray } from "../src/health.js";
+
+test("Check sort array", () => {
+  const array = [
+    { name: "мечник", health: 10 },
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+  ];
+  const arrayExpected = [
+    { name: "маг", health: 100 },
+    { name: "лучник", health: 80 },
+    { name: "мечник", health: 10 },
+  ];
+  const result = sortArray(array);
+  expect(result).toEqual(arrayExpected);
+});
